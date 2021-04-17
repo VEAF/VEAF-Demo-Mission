911f9bc88db7a96b6a157e6153c0fc7f8cf0326a
David Pierron <dpierron@gmail.com>
2021-04-17 11:06:29 +0200

    updated scripts

--- build-zip.cmd
+++ build-zip.cmd
@@ -1,6 +1,7 @@
 @echo off
 set NOPAUSE=true
 set VERBOSE_LOG_FLAG=true
+set SECURITY_DISABLED_FLAG=true
 set DYNAMIC_SCRIPTS_PATH="d:\dev\_VEAF\VEAF-Mission-Creation-Tools"
 call build.cmd
 copy build\*.miz .
